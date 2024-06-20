1201371c9cbaa6d190fc9aba9bf894607ed5ce88
eugustaelix <gustavo.silva2116@etec.sp.gov.br>
2024-06-20 16:29:12 -0300

    Meu primeiro comit

---
+++ API_AulaMobile/API_AulaMobile/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace API_AulaMobile.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ API_AulaMobile/API_AulaMobile/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API_AulaMobile.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
